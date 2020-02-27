f569d2ddf8cef25d2c8dcbdd26eddb86eeef0736
Maicon <maiconwesleandro@gmail.com>
2020-02-26 21:33:45 -0300

    Layout Finished

---
+++ distribute-teams/app/src/main/java/br/com/example/distributeteams/Teams.kt
@@ -0,0 +1,13 @@
+package br.com.example.distributeteams
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Teams : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_teams)
+
+    }
+}

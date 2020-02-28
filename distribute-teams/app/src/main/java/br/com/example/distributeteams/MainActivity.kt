637a33fc8fe0aa4eb464136a35b064fcc7d808aa
Maicon <maiconwesleandro@gmail.com>
2020-02-27 21:13:07 -0300

    Finished App

--- distribute-teams/app/src/main/java/br/com/example/distributeteams/MainActivity.kt
+++ distribute-teams/app/src/main/java/br/com/example/distributeteams/MainActivity.kt
@@ -1,12 +1,28 @@
 package br.com.example.distributeteams
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.sax.StartElementListener
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonNext.setOnClickListener {
+
+            //go to next activity
+            val intent = Intent(applicationContext, Teams::class.java)
+            //send values
+            intent.putExtra("player1", editTextPlay1.text.toString())
+            intent.putExtra("player2", editTextPlay2.text.toString())
+            intent.putExtra("player3", editTextPlay3.text.toString())
+            intent.putExtra("player4", editTextPlay4.text.toString())
+            intent.putExtra("player5", editTextPlay5.text.toString())
+            startActivity(intent)
+        }
     }
 }
